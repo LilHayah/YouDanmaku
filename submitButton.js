9c91d09d7e03117cd7286938e6116c9556dd941b
Truynes <den.senyurt@gmail.com>
2015-03-28 17:33:32 -0400

    Create submitButton.js

---
+++ submitButton.js
@@ -0,0 +1,32 @@
+var btnSubmit = 'Submit-Comment';
+
+function checkButton(){
+	var button = document.getElementById(btnSubmit);
+
+	if (button == null){
+		add button();
+	}
+		
+}
+
+function addButton(){
+	var container = document.getElementById('watch7-user-header');
+	var btn = document.createElement('button');
+	lastContainerChild = container.lastElementChild;
+	btn.id = 'comment';
+	btn.setAttribute('type', 'button');
+	btn.setAttribute('Title', 'Submit');
+	btn.setAttribute('data-tool-tip', 'Submit');
+	btn.setAttribute('class', 'yt-subscription-button yt-uix-button yt-uix-button-subscribe-branded');
+	btn.style.marginleft='10px';
+
+
+	var btnTitle = document.createElement('span');
+	btnTitle.setAttribute('id', 'submit');
+	btnTitle.appendChild(document.createTextNode('Submit!'));
+	btnTitle.setAttribute('class', 'yt-uix-button-content');
+	btn.appendChild(btnTitle);
+	container.appendChild(btn);
+}
+
+window.setInterval(checkButton,1000);
