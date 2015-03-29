40f50926392192378c92ef89dc438a0a64cf1e19
Tian Zhou <tianzhou93@gmail.com>
2015-03-29 06:32:09 -0400

    Change timestamp to number of seconds

--- main.js
+++ main.js
@@ -14,16 +14,25 @@ function insert_comment_block() {
 
     $("#submit").on("click", function(e) {
         e.preventDefault();
+        
         var videoId = location.href.substr(location.href.indexOf("=") + 1);
-        var timestamp = document.getElementsByClassName("ytp-time-current")[0].innerHTML;
+        
+        var timestamp;
+        var time = document.getElementsByClassName("ytp-time-current")[0].innerHTML.split(":");
+        if (time.length == 2) {
+            timestamp = Number(time[0]) * 60 + Number(time[1]);
+        }
+        else {
+            timestamp = Number(time[0] * 3600) + Number(time[1]) * 60 + Number(time[2]);
+        }
+        
         var comment = document.getElementById("comment").value;
-
         if (comment.length > 140) {
             alert("Too many characters!");
             return false;
         }
         var dataString = 'VideoId='+ videoId + '&Timestamp='+ timestamp + '&Comment='+ comment;
-        
+
         $.ajax({
             type: "POST",
             url: "https://youtubecomment.azurewebsites.net/youtube/upload.php",
@@ -46,4 +55,3 @@ document.addEventListener("DOMSubtreeModified", function() {
     }
     timeout = setTimeout(checkURL, 500);
 }, false);
-
